23ef2b554a689f1da24a77107d883311ab179f7b
Chris Cundill <chrisc@diligentsoft.co>
2024-08-13 12:15:31 +0100

    Disabled logging of access token for now due to decryption problem

--- mwaa_authx_lambda_function.py
+++ mwaa_authx_lambda_function.py
@@ -38,9 +38,9 @@ def lambda_handler(event, context):
         path = event['path']
         headers = event['multiValueHeaders']
 
-        if 'x-amzn-oidc-accesstoken' in headers:
-            access_token = decode_jwt(headers['x-amzn-oidc-accesstoken'][0])
-            logger.info('Access token payload: {}'.format(access_token))
+        # if 'x-amzn-oidc-accesstoken' in headers:
+        #     access_token = decode_jwt(headers['x-amzn-oidc-accesstoken'][0])
+        #     logger.info('Access token payload: {}'.format(access_token))
 
         if 'x-amzn-oidc-data' in headers:
             encoded_jwt = headers['x-amzn-oidc-data'][0]
